749aa068b422f46b2873e0071595f82660bef170
AdrianDrevland <Adrian.l.drevland@gmail.com>
2020-09-15 15:03:25 +0200

    cleaned up code

--- TicTacToe/Source.cpp
+++ TicTacToe/Source.cpp
@@ -2,27 +2,26 @@
 #include <array>
 #include <string>
 #include <stdlib.h>
-
+#include <vector>
 //clean the code
 
 //global vars
+//tried char. didn't like it, string works fine.
 std::string box[] = { "1","2","3","4","5","6","7","8","9" };
-std::string box_clone[] = { "1","2","3","4","5","6","7","8","9" };
+//std::vector <char> box_vector{ '1','2','3','4','5','6','7','8','9' };
 int taken[10];
 
-
 void clear_board() {
-	for (int k = 0; k < 10; k++)
+	for (int k = 0; k < _countof(taken); k++)
 	{
 		// clears box array to "default" [1-9]
 		std::string pls_work = std::to_string(k+1);
 		box[k] = pls_work;
-
 		//clears taken[]
 		taken[k] = 0;
 	}
-	
 }
+
 // checks for win. horizontally, vertical and diagonal. returns who won
 std::string check_win() {
 	if (box[0] == box[1] && box[1] == box[2])
@@ -42,7 +41,6 @@ std::string check_win() {
 	else if (box[2] == box[4] && box[4] == box[6])
 		return box[2];
 
-
 	//check for tie. returns "t" to call out a tie.
 	int count_taken{ 0 };
 	for (int i = 0; i < 10; i++)
@@ -54,13 +52,14 @@ std::string check_win() {
 	}
 	return "none";
 }
+
 int choose(int player) {
 	int p_chose;
-	std::cout << "player " << player << " choose a number [1-9]: ";
+	std::cout << "\n\n player " << player << " choose a number [1-9]: ";
 	std::cin >> p_chose;
 
 	//checks if space is taken
-	for (int a{1}; a < 10; a++)
+	for (int a{1}; a < _countof(taken); a++)
 	{
 		if (p_chose == taken[a])
 		{
@@ -69,90 +68,47 @@ int choose(int player) {
 			std::cin >> p_chose;
 		}
 	}
-
 	taken[p_chose] = p_chose;
-
 	if (player == 1)
-	{
 		box[p_chose -1] = "X";
-		box_clone[p_chose - 1] = "X";
-	}
 	else if (player == 2)
-	{
 		box[p_chose - 1] = "O";
-		box_clone[p_chose - 1] = "O";
-	}
-	
 	return 0;
 }
 
 // checks if space is taken
 bool check_taken(int placement) {
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < _countof(taken); i++)
 	{
 		if (placement == taken[i])
-		{
 			return true;
-			// std::cout << "place taken.";
-		}
 	}
 	return false;
 }
 
-// computer checks if random spot is taken. if not then places an "O" in that spot.
-void easy_ai_choose() {
-	int placement{1};
-	do
-	{
-		placement = rand() % 9 + 1;
-		//std::cout << placement << std::endl;
-	} while (check_taken(placement) == true);
-	box[placement-1] = "O";
-	taken[placement] = placement;
-}
-
-//checking score for spesifide input
-//dont need depth
-int minimax(int check_number, int depth, bool ai_turn) {
-	int depth2 = depth;
+//checking score for specific input
+int minimax(int check_number, bool ai_turn) {
+	//places O or X depends on whos turn
 	if (ai_turn == true) 
-	{
 		box[check_number - 1] = "O";
-	}
 	else
-	{
 		box[check_number - 1] = "X";
-	}
-
+	//checks for win then removes the last placed X or O
 	int score{-99};
 	std::string result = check_win();
-	std::cout << "check win for nr: "<<check_number << " result is : " << result << std::endl;
 	box[check_number - 1] = std::to_string(check_number);
-	std::cout << "dept : " << depth2 << std::endl;
 
 	//give points depending of the next move
 	if (result == "t")
-	{
 		return 0;
-	}
 	else if (result == "O")
-	{
 		return 10;
-	}
 	else if (result == "X")
-	{
 		return 5; 
-	}
-// 	else if (depth2 >= 3) 
-// 	{
-// 		return -1;
-// 	}
  	else if (result == "none")
- 	{
 		return -5; 
- 	}
 
-	// maximizing player. ai
+	// maximizing player. ai score
 	if (ai_turn == true)
 	{
 		int best_score{ -99 };
@@ -161,17 +117,14 @@ int minimax(int check_number, int depth, bool ai_turn) {
 		{
 			if (check_taken(i) == false)
 			{
-				int score = minimax(check_number, depth2, false);
+				int score = minimax(check_number, false);
 				if (score > best_score)
-				{
 					best_score = score;
-				}
 			}
 		}
-		std::cout << "Ai's turns \n";
 		return best_score;
 	}
-	// minimizing player, player
+	// minimizing player, players score
  	else
  	{
  		int best_score{ 99 };
@@ -179,28 +132,25 @@ int minimax(int check_number, int depth, bool ai_turn) {
  		{
  			if (check_taken(i) == false)
  			{
- 				int score = minimax(check_number, depth2++, true);
+ 				int score = minimax(check_number, true);
  				if (score < best_score)
- 				{
  					best_score = score;
- 				}
  			}
  		}
-		std::cout << "humans turns \n";
  		return best_score;
  	}
 	return -1;
 }
 
 int best_move(bool ai_turn) {
-	int best_score = -99;
+	int best_score = -99; // a tiny number
 	int chose;
 	//looks for first available spot
 	for (int i = 1; i < 10; i++)
 	{
 		if (check_taken(i) == false)
 		{
-			int score = minimax(i, 1, ai_turn);
+			int score = minimax(i, ai_turn);
 			std::cout << "the score is: " << score << std::endl;
 			if (score > best_score)
 			{
@@ -209,10 +159,8 @@ int best_move(bool ai_turn) {
 				
 			}
 		}
-		std::cout << "best score is currently: " << best_score << std::endl;
 	}
 	return chose;
-	
 }
 // ai places O
 void bot_choose(int place) {
@@ -223,30 +171,26 @@ void bot_choose(int place) {
 
 
 //draws game board
-void board() {
-
-	std::cout << "\t-------------\n";
-	std::cout << "\t| " << box[0] << " | " << box[1] << " | " << box[2] << " |\n";
-	std::cout << "\t-------------\n";
-	std::cout << "\t| " << box[3] << " | " << box[4] << " | " << box[5] << " |\n";
-	std::cout << "\t-------------\n";
-	std::cout << "\t| " << box[6] << " | " << box[7] << " | " << box[8] << " |\n";
-	std::cout << "\t-------------\n";
+void board() 
+{
+	for (int i = 0; i < _countof(box) ; i++)
+	{
+		if (i % 3 == 0)
+			std::cout << "\n\t-------------\n\t| ";
+		std::cout << box[i] << " | ";
+	}
+	std::cout << "\n\t-------------\n";
 }
 
-//compares score of human and ai spot
+//compares score of human and ai choses
 int compare_score(int human, int ai) {
-	int human_score = minimax(human, 0, false);
-	int ai_score = minimax(ai, 0, true);
+	int human_score = minimax(human, false);
+	int ai_score = minimax(ai, true);
 
 	if (human_score > ai_score)
-	{
 		return 1;
-	}
 	else
-	{
 		return 0;
-	}
 }
 
 // using minimax algorithm, very hard to win
@@ -277,13 +221,9 @@ void impossible() {
 		int human_move = best_move(false);
 		std::cout << "Best human move: " << human_move << std::endl;
 		if (compare_score(human_move, ai_move) == 1)
-		{
 			move = human_move;
-		}
 		else
-		{
 			move = ai_move;
-		}
 		bot_choose(move);
 		system("cls");
 
@@ -303,43 +243,6 @@ void impossible() {
 	}
 }
 
-//start game for 1 player. ai is only random
-// void players_1() {
-// 	while (true)
-// 	{
-// 		board();
-// 		choose(1);
-// 		system("cls");
-// 		if (check_win() == "O")
-// 		{
-// 			std::cout << "Congrats you won!!\n";
-// 			board();
-// 			break;
-// 		}
-// 		else if (check_win() == "t")
-// 		{
-// 			std::cout << "Well played, its a tie!\n";
-// 			board();
-// 			break;
-// 		}
-// 		board();
-// 		easy_ai_choose();
-// 		system("cls");
-// 		if (check_win() == "X")
-// 		{
-// 			std::cout << "you just got beaten by a random bot\n";
-// 			board();
-// 			break;
-// 		}
-// 		else if (check_win() == "t")
-// 		{
-// 			std::cout << "Well played, its a tie!\n";
-// 			board();
-// 			break;
-// 		}
-// 	}
-// }
-//starts game for 2 players
 void players_2() {
 	while (true)
 	{
@@ -399,11 +302,6 @@ int main() {
 			clear_board();
 			players_2();
 			break;
-//  		case 3: // this is for testing purposes
-// 			system("cls");
-// 			clear_board();
-// 			impossible();
-//  			break;
 		default:
 			break;
 		}
@@ -419,6 +317,5 @@ int main() {
 		if (yn == 'n')
 		break;
 	} while (true);
-
 	return 0;
 }

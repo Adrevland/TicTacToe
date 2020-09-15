29759cd34b154186f8bfd1f473a5daddcc648157
AdrianDrevland <Adrian.l.drevland@gmail.com>
2020-09-15 15:28:31 +0200

    really short!!!

--- TicTacToe/Source.cpp
+++ TicTacToe/Source.cpp
@@ -10,8 +10,7 @@ int taken[10];
 
 void clear_board() {
 	// clears box array to "default" [1-9]
-	for (int k = 0; k < _countof(box); k++)
-	{
+	for (int k = 0; k < _countof(box); k++){
 		std::string pls_work = std::to_string(k+1);
 		box[k] = pls_work;
 	}
@@ -19,7 +18,6 @@ void clear_board() {
 	for (int j = 0; j < _countof(taken); j++)
 		taken[j] = 0;
 }
-
 // checks for win. horizontally, vertical and diagonal. returns who won
 std::string check_win() {
 	if (box[0] == box[1] && box[1] == box[2])
@@ -41,8 +39,7 @@ std::string check_win() {
 
 	//check for tie. returns "t" to call out a tie.
 	int count_taken{ 0 };
-	for (int i = 0; i < 10; i++)
-	{
+	for (int i = 0; i < 10; i++){
 		if (box[i] == "O" || box[i] == "X")
 			count_taken++;
 		else if (count_taken >= 9)
@@ -50,17 +47,13 @@ std::string check_win() {
 	}
 	return "none";
 }
-
 int choose(int player) {
 	int p_chose;
 	std::cout << "\n\n player " << player << " choose a number [1-9]: ";
 	std::cin >> p_chose;
-
 	//checks if space is taken
-	for (int a{1}; a < _countof(taken); a++)
-	{
-		if (p_chose == taken[a])
-		{
+	for (int a{1}; a < _countof(taken); a++){
+		if (p_chose == taken[a]){
 			std::cout << "Place is taken\n";
 			std::cout << "player " << player << " choose a number [1-9]: ";
 			std::cin >> p_chose;
@@ -73,17 +66,14 @@ int choose(int player) {
 		box[p_chose - 1] = "O";
 	return 0;
 }
-
 // checks if space is taken
 bool check_taken(int placement) {
-	for (int i = 0; i < _countof(taken); i++)
-	{
+	for (int i = 0; i < _countof(taken); i++){
 		if (placement == taken[i])
 			return true;
 	}
 	return false;
 }
-
 //checking score for specific input
 int minimax(int check_number, bool ai_turn) {
 	//places O or X depends on whos turn
@@ -107,13 +97,10 @@ int minimax(int check_number, bool ai_turn) {
 		return -5; 
 
 	// maximizing player. ai score
-	if (ai_turn == true)
-	{
+	if (ai_turn == true){
 		int best_score{ -99 };
-		for (int i = 0; i < 9; i++)
-		{
-			if (check_taken(i) == false)
-			{
+		for (int i = 0; i < 9; i++){
+			if (check_taken(i) == false){
 				int score = minimax(check_number, false);
 				if (score > best_score)
 					best_score = score;
@@ -122,13 +109,10 @@ int minimax(int check_number, bool ai_turn) {
 		return best_score;
 	}
 	// minimizing player, players score
- 	else
- 	{
+ 	else{
  		int best_score{ 99 };
- 		for (int i = 0; i < 9; i++)
- 		{
- 			if (check_taken(i) == false)
- 			{
+ 		for (int i = 0; i < 9; i++){
+ 			if (check_taken(i) == false){
  				int score = minimax(check_number, true);
  				if (score < best_score)
  					best_score = score;
@@ -138,19 +122,15 @@ int minimax(int check_number, bool ai_turn) {
  	}
 	return -1;
 }
-
 int best_move(bool ai_turn) {
 	int best_score = -99; // a tiny number
 	int chose;
 	//looks for first available spot
-	for (int i = 1; i < 10; i++)
-	{
-		if (check_taken(i) == false)
-		{
+	for (int i = 1; i < 10; i++){
+		if (check_taken(i) == false){
 			int score = minimax(i, ai_turn);
 			std::cout << "the score is: " << score << std::endl;
-			if (score > best_score)
-			{
+			if (score > best_score){
 				chose = i;
 				best_score = score;
 			}
@@ -163,46 +143,36 @@ void bot_choose(int place) {
 	box[place-1] = "O";
 	taken[place] = place;
 }
-
-
-
 //draws game board
 void board() {
-	for (int i = 0; i < _countof(box) ; i++)
-	{
+	for (int i = 0; i < _countof(box) ; i++){
 		if (i % 3 == 0)
 			std::cout << "\n\t-------------\n\t| ";
 		std::cout << box[i] << " | ";
 	}
 	std::cout << "\n\t-------------\n";
 }
-
 //compares score of human and ai choses
 int compare_score(int human, int ai) {
 	int human_score = minimax(human, false);
 	int ai_score = minimax(ai, true);
-
 	if (human_score > ai_score)
 		return 1;
 	else
 		return 0;
 }
-
 // using minimax algorithm, very hard to win
 void impossible(){
-	while (check_win() == "none")
-	{
+	while (check_win() == "none"){
 		board();
 		choose(1);
 		system("cls");
-		if (check_win() == "X")
-		{
+		if (check_win() == "X"){
 			std::cout << "Very well, You are a lucky player\n";
 			board();
 			break;
 		}
-		else if (check_win() == "t")
-		{
+		else if (check_win() == "t"){
 			std::cout << "Well played, its a tie!\n";
 			board();
 			break;
@@ -219,37 +189,29 @@ void impossible(){
 			move = ai_move;
 		bot_choose(move);
 		system("cls");
-
-		if (check_win() == "O")
-		{
+		if (check_win() == "O"){
 			std::cout << "You lost. but its fine. u cant win\n";
 			board();
 			break;
 		}
-		else if (check_win() == "t")
-		{
+		else if (check_win() == "t"){
 			std::cout << "Well played, its a tie!\n";
 			board();
 			break;
 		}
-		
 	}
 }
-
 void players_2() {
-	while (true)
-	{
+	while (true){
 		board();
 		choose(1);
 		system("cls");
-		if (check_win() == "X")
-		{
+		if (check_win() == "X"){
 			std::cout << "Congrats player 1! you won!!\n";
 			board();
 			break;
 		}
-		else if (check_win() == "t")
-		{
+		else if (check_win() == "t"){
 			std::cout << "Well played, its a tie!\n";
 			board();
 			break;
@@ -257,33 +219,28 @@ void players_2() {
 		board();
 		choose(2);
 		system("cls");
-		if (check_win() == "O")
-		{
+		if (check_win() == "O"){
 			std::cout << "Congrats player 2! you won!!\n";
 			board();
 			break;
 		}
-		else if (check_win() == "t")
-		{
+		else if (check_win() == "t"){
 			std::cout << "Well played, its a tie!\n";
 			board();
 			break;
 		}
 	}
 }
-
 int main() {
 	system("cls");
 	int player_count;
 	char yn;
 	bool new_game = true;
 	std::cout << "\n***Welcome to 1 and 2 player tic tac toe***" << std::endl;
-	do 
-	{
+	do {
 		std::cout << "\nhow many players want to play? [1-2]: ";
 		std::cin >> player_count;
-		switch (player_count)
-		{
+		switch (player_count){
 		case 1:
 			system("cls");
 			clear_board();
@@ -297,8 +254,7 @@ int main() {
 		default:
 			break;
 		}
-		do
-		{
+		do{
 			std::cout << "\nDo you want to play another one ? [y/n]: ";
 			std::cin >> yn;
 			if (yn == 'y' || yn == 'n')
